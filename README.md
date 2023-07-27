7427cba1a3ef20346be1c6eeaaaaaa1e6dd76010
JUNZHU-SEIS <Jun__Zhu@outlook.com>
2023-07-27 21:10:37 +0800

    v0.1

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # USTC-Pickers: a Unified Set of seismic phase pickers Transfer learned for China
 
+Update summary for v0.1:
+1. The model is the same as the original [PhaseNet](https://github.com/AI4EPS/PhaseNet/blob/master/phasenet/model.py) implemented with TensorFlow by W. Zhu&G. Beroza (2018). The earlier version (called PhaseNetLight, see this [PR](https://github.com/seisbench/seisbench/pull/158) for more details) will not be maintained any longer. A comparison between the output of the old and this new model is shown below (The input waveform is from Sichuan province).
+2. We adopt diverse data augmentation techniques when training the China model: adding data gaps, superimposing waveforms with the pure Noise from [STEAD](https://github.com/smousavi05/STEAD), randomly dropping 1 or 2 of the 3 components, waveform clipping, random waveform shifting.
+![old](https://raw.githubusercontent.com/JUNZHU-SEIS/USTC-Pickers/main/demo/figure/test.png)
+![new](./demo/figure/Sichuan_New_model.png)
+
 # 1. Install [Anaconda](https://www.anaconda.com/) and requirements
 
 * Download USTC-Pickers repository
@@ -32,3 +38,32 @@ Located in the directory: **USTC-Pickers/model_list/**
 # 3. Batch prediction
 
 See details in the [Notebook](https://github.com/JUNZHU-SEIS/USTC-Pickers/blob/main/demo/demo_pick.ipynb)
+
+# 4. Citation
+If you find this toolkit helpful, please cite papers below:
+
+---
+
+* [USTC-Pickers: a Unified Set of seismic phase pickers Transfer learned for China](https://www.equsci.org.cn/article/doi/10.1016/j.eqs.2023.03.001?pageType=en)
+
+  _Suggestions on how to use USTC-Pickers._
+---
+
+---
+
+* [SeisBench - A Toolbox for Machine Learning in Seismology](https://doi.org/10.1785/0220210324)
+
+  _Reference publication for software._
+
+---
+
+* [Which picker fits my data? A quantitative evaluation of deep learning based seismic pickers](https://doi.org/10.1029/2021JB023499)
+
+  _Example of in-depth bencharking study of deep learning-based picking routines using the SeisBench framework._
+
+---
+# 5. Other tutorials
+### Video
+[Introduction](https://www.koushare.com/video/videodetail/31654) and [manual](https://www.koushare.com/video/videodetail/31655)
+### Slide
+[2022_ustc_seis_workshop.pptx](http://home.ustc.edu.cn/~zhujun2316/paper/2022_ustc_seis_workshop.pptx)
--- demo/pick.py
+++ demo/pick.py
@@ -1,26 +1,30 @@
 import seisbench
 import seisbench.models as sbm
 import torch
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
 import numpy as np
 from obspy import read
 import matplotlib.pyplot as plt
 from config import *
 
-# 模型初始化
-picker = sbm.PhaseNet(phases='PSN', sampling_rate=sample_rate)
 # 为拾取的目标区域选定合适的picker，如Sichuan（四川）、CSES（实验场）、TP（青藏高原）、China（中国）
-location = input('Plase specify a region or province to pick phases, e.g. Beijing. 41 pickers are available in the subfolder "%s"'%os.path.join('USTC-Pickers', 'model_list', ''))
+location = input('Plase specify a region or province to pick phases, e.g. Beijing. 42 pickers are available in the subfolder "%s"'%os.path.join('USTC-Pickers', 'model_list', 'v0.1'))
 if location not in en2cn:
     exit('The region you specified is not available. Plase choose a region or province from below--------\n%s\n-----------------------------------------------------------------------------------------------'%(', '.join(en2cn)))
-else:
+elif location!='China':
     model_save_path = glob.glob(os.path.join(model_list, '*'+en2cn[location]+'.pt'))[0]
-print('You are using the picker located at %s\n'%model_save_path)
-# 加载模型
-picker.load_state_dict(torch.load(model_save_path,
+    print('You are using the picker located at %s\n'%model_save_path)
+    # 模型初始化
+    picker = sbm.PhaseNet(sampling_rate=sample_rate)
+    # 加载模型
+    picker.load_state_dict(torch.load(model_save_path,
 			map_location=device).state_dict())
+else:
+    print('You are using the China picker, "diting", published by SeisBench')
+    picker = sbm.PhaseNet.from_pretrained('diting')
+    picker.sampling_rate=sample_rate
 
-# 读取波G形
+# 读取波形
 mseed = mseed.replace('Beijing', 'Sichuan')
 sac = sac.replace('Beijing', 'Sichuan')
 inputfile = mseed
@@ -30,16 +34,19 @@ print(stream, '\n')
 
 # 模型响应（非必要）
 response = picker.annotate(stream)
-response.plot()
+time_diff = response[0].stats.starttime-stream[0].stats.starttime
+print(response)
+#response.plot()
 fig, ax = plt.subplots(2, 1, sharex=True)
-t = np.arange(response[0].stats.npts)/sample_rate
-ax[0].plot(t, np.array([x.data for x in stream]).transpose(), label=[x.stats.channel for x in stream])
-ax[1].plot(t, np.array([x.data for x in response[:-1]]).transpose(), label=['P', 'S'])
+t0 = np.arange(stream[0].stats.npts)/sample_rate
+t1 = np.arange(response[0].stats.npts)/sample_rate + time_diff
+ax[0].plot(t0, np.array([x.data for x in stream]).transpose(), label=[x.stats.channel for x in stream])
+ax[1].plot(t1, np.array([x.data for x in response[1:]]).transpose(), label=['P', 'S'])
 ax[1].set_xlabel('Time (s)')
 ax[1].set_ylim(0, 1)
 for j in ax:
     j.legend()
-    j.set_xlim(0, t.max()+1)
+    j.set_xlim(0, t0.max()+1)
 plt.tight_layout()
 plt.savefig('figure/test.png', dpi=1000)
 plt.show()
